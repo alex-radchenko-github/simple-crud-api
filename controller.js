e2301339ad5cfd4b9755ddf425ee3c36021197bf
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2021-11-27 11:28:23 -0500

    new init commit

---
+++ controller.js
@@ -0,0 +1,73 @@
+const data = require("./data");
+const {v4, validate} = require('uuid')
+
+
+class Controller {
+    async getAllUsers() {
+        return new Promise((resolve, _) => resolve(data));
+    }
+
+    async getTodo(id) {
+        return new Promise((resolve, reject) => {
+            let user
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].id === id) {
+                    user = data[i]
+                }
+            }
+
+
+            if (user) {
+                resolve(user);
+            } else {
+                reject(`User with id ${id} not found `);
+            }
+        });
+    }
+
+    // creating a todo
+    async createTodo(user) {
+        return new Promise((resolve, _) => {
+            let newUser = {
+                id: v4(),
+                ...user,
+            }
+            data.push(newUser)
+            // return the new created todo
+            resolve(newUser);
+
+        });
+    }
+
+    // updating a todo
+    async updateTodo(id) {
+        return new Promise((resolve, reject) => {
+            // get the todo.
+            let todo = data.find((todo) => todo.id === parseInt(id));
+            // if no todo, return an error
+            if (!todo) {
+                reject(`No todo with id ${id} found`);
+            }
+            //else, update it by setting completed to true
+            todo["completed"] = true;
+            // return the updated todo
+            resolve(todo);
+        });
+    }
+
+    // deleting a todo
+    async deleteTodo(id) {
+        return new Promise((resolve, reject) => {
+            // get the todo
+            let todo = data.find((todo) => todo.id === parseInt(id));
+            // if no todo, return an error
+            if (!todo) {
+                reject(`No todo with id ${id} found`);
+            }
+            // else, return a success message
+            resolve(`Todo deleted successfully`);
+        });
+    }
+}
+
+module.exports = Controller;
--- index.js
+++ index.js
@@ -1,21 +1,112 @@
-const Router = require("./src/Router.js");
-const Application = require("./src/Application");
+const http = require("http");
+const Todo = require("./controller");
+const {getReqData} = require("./utils");
+const {v4, validate} = require('uuid')
+
+
 const PORT = process.env.PORT || 5000;
 
-const app = new Application()
+const server = http.createServer(async (req, res) => {
 
-const router = new Router();
+    if (req.url === "/person" && req.method === "GET") {
+        const todos = await new Todo().getAllUsers();
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify(todos));
+    } else if (req.url === "/person/" + req.url.split("/")[2] && req.method === "GET") {
 
-router.get('/person', (req, res) => {
-    res.end('path /person')
-})
+        try {
+            const id = req.url.split("/")[2];
+            if (!validate(id)) {
+                res.writeHead(400, {'Content-Type': 'text/plain'});
+                return res.end(`Error: Not valid ID`);
 
-app.addRouter(router)
+            } else {
+                // console.log(id)
+                // get todo
+                const todo = await new Todo().getTodo(id);
+                // set the status code and content-type
+                res.writeHead(200, {"Content-Type": "application/json"});
+                // send the data
+                res.end(JSON.stringify(todo));
 
-app.listen(PORT, () => console.log(`Server start on ${PORT}`))
 
-// const {v4, validate} = require('uuid');
-// console.log(v4())
-// console.log(validate('14b59bef-d968-4043-842d-d5d9f92179c8'))
-// 14b59bef-d968-4043-842d-d5d9f92179 c8
+            }
 
+        } catch (error) {
+            // set the status code and content-type
+            res.writeHead(404, {"Content-Type": "application/json"});
+            // send the error
+            res.end(JSON.stringify({message: error}));
+        }
+    }
+
+    // /api/todos/:id : DELETE
+    else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "DELETE") {
+        try {
+            // get the id from url
+            const id = req.url.split("/")[2];
+            // delete todo
+            let message = await new Todo().deleteTodo(id);
+            // set the status code and content-type
+            res.writeHead(200, {"Content-Type": "application/json"});
+            // send the message
+            res.end(JSON.stringify({message}));
+        } catch (error) {
+            // set the status code and content-type
+            res.writeHead(404, {"Content-Type": "application/json"});
+            // send the error
+            res.end(JSON.stringify({message: error}));
+        }
+    }
+
+    // /api/todos/:id : UPDATE
+    else if (req.url.match(/\/api\/todos\/([0-9]+)/) && req.method === "PATCH") {
+        try {
+            // get the id from the url
+            const id = req.url.split("/")[2];
+            // update todo
+            let updated_todo = await new Todo().updateTodo(id);
+            // set the status code and content-type
+            res.writeHead(200, {"Content-Type": "application/json"});
+            // send the message
+            res.end(JSON.stringify(updated_todo));
+        } catch (error) {
+            // set the status code and content type
+            res.writeHead(404, {"Content-Type": "application/json"});
+            // send the error
+            res.end(JSON.stringify({message: error}));
+        }
+    } else if (req.url === "/person" && req.method === "POST") {
+        try {
+            let todo_data = await getReqData(req);
+            let todo = await new Todo().createTodo(JSON.parse(todo_data));
+            res.writeHead(201, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(todo));
+
+        } catch (e) {
+            res.writeHead(500, {"Content-Type": "application/json"});
+            //send the todo
+            res.end(JSON.stringify('500 ошибка'));
+        }
+
+
+        // // get the data sent along
+        // let todo_data = await getReqData(req);
+        // // create the todo
+        // let todo = await new Todo().createTodo(JSON.parse(todo_data));
+        // // set the status code and content-type
+        // res.writeHead(201, {"Content-Type": "application/json"});
+        // //send the todo
+        // res.end(JSON.stringify(todo));
+    }
+
+    // No route present
+    else {
+        res.writeHead(404, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({message: "Route not found"}));
+    }
+});
+
+server.listen(PORT, () => {
+    console.log(`server started on port: ${PORT}`);
+});
