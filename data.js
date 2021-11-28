c7638e2442d0696bc936ccac83524eff459ba5ee
alex-radchenko-github <73553196+alex-radchenko-github@users.noreply.github.com>
2021-11-27 23:21:00 -0500

    delete data

--- data.js
+++ data.js
@@ -1,15 +1,2 @@
-const users = [
-    // {
-    //     "id": "14b59bef-d968-4043-842d-d5d9f92179c8",
-    //     "name": "111",
-    //     "age": 111,
-    //     "hobbies": ["111", "111"]
-    // },
-    // {
-    //     "id": "7d761355-cf6f-48a9-985d-61be61f2363f",
-    //     "name": "222",
-    //     "age": 222,
-    //     "hobbies": ["222", "222"]
-    // }
-];
+const users = [];
 module.exports = users;
